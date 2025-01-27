05da6b6ff59a790c5cd76260005eb922c74b83fb
saeedurrehman101 <saeedtutorial786@gmail.com>
2025-01-27 04:35:46 -0100

    Encapsulation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapsulation Obj = new Encapsulation();
+            Obj.Name = "Ali";
+            Obj.Age = 100;
+            Console.WriteLine(Obj.Name);
+            Console.WriteLine(Obj.Age);
+        }
+    }
+}
